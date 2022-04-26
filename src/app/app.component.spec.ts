966f461d80a1c0da6e313471b8b2607f56b570df
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2022-04-26 15:48:44 +0300

    feat(ws): add prettier & eslint & husky & lint-staged [WiP]

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,35 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-
-describe('AppComponent', () => {
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+// eslint-disable-next-line no-undef
+describe("AppComponent", () => {
+  // eslint-disable-next-line no-undef
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   });
-
-  it('should create the app', () => {
+  // eslint-disable-next-line no-undef
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    // eslint-disable-next-line no-undef
     expect(app).toBeTruthy();
   });
-
+  // eslint-disable-next-line
   it(`should have as title 'ng-starter-kit'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-starter-kit');
+    // eslint-disable-next-line
+    expect(app.title).toEqual("ng-starter-kit");
   });
-
-  it('should render title', () => {
+  // eslint-disable-next-line
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-starter-kit app is running!');
+    // eslint-disable-next-line
+    expect(compiled.querySelector(".content span")?.textContent).toContain(
+      "ng-starter-kit app is running!"
+    );
   });
 });
